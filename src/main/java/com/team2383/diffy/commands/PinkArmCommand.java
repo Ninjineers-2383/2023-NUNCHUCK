632aa95a7d321bf5a572ddb49848b8ebb1fa9bf9
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2023-01-15 15:12:27 -0500

    Added Pink Arm Command

---
+++ src/main/java/com/team2383/diffy/commands/PinkArmCommand.java
@@ -0,0 +1,37 @@
+package com.team2383.diffy.commands;
+
+import java.util.function.DoubleSupplier;
+
+import com.team2383.diffy.subsystems.PinkArmSubsystem;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PinkArmCommand extends CommandBase {
+    private PinkArmSubsystem m_pinkArm;
+
+    private DoubleSupplier m_bottomAngle;
+    private DoubleSupplier m_bottomSpeed;
+    private DoubleSupplier m_extension;
+    private DoubleSupplier m_extensionSpeed;
+    private DoubleSupplier m_topAngle;
+    private DoubleSupplier m_topSpeed;
+
+    public PinkArmCommand(PinkArmSubsystem pinkArm, DoubleSupplier desiredBottomAngle, DoubleSupplier desiredBottomSpeed, DoubleSupplier desiredExtension, 
+        DoubleSupplier desiredExtensionSpeed, DoubleSupplier desiredTopAngle, DoubleSupplier desiredTopSpeed) {
+        m_pinkArm = pinkArm;
+        m_bottomAngle = desiredBottomAngle;
+        m_bottomSpeed = desiredBottomSpeed;
+        m_extension = desiredExtension;
+        m_extensionSpeed = desiredExtensionSpeed;
+        m_topAngle = desiredTopAngle;
+        m_topSpeed = desiredTopSpeed;
+
+        addRequirements(m_pinkArm);
+    }
+
+    @Override
+    public void execute() {
+        m_pinkArm.setDesiredState(m_bottomAngle.getAsDouble(), m_bottomSpeed.getAsDouble(), 
+            m_extension.getAsDouble(), m_extensionSpeed.getAsDouble(), m_topAngle.getAsDouble(), m_topSpeed.getAsDouble());
+    }
+}
---
+++ src/main/java/com/team2383/diffy/subsystems/PinkArmSubsystem.java
@@ -0,0 +1,42 @@
+package com.team2383.diffy.subsystems;
+
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PinkArmSubsystem extends SubsystemBase {
+    private BottomPivotSubsystem m_bottomPivot;
+    private TelescopeSubsystem m_telescope;
+    private TopPivotSubsystem m_topPivot;
+
+    private DataLog m_log;
+
+    public PinkArmSubsystem(DataLog log) {
+        m_log = log;
+
+        m_bottomPivot = new BottomPivotSubsystem(m_log);
+        m_telescope = new TelescopeSubsystem(m_log);
+        m_topPivot = new TopPivotSubsystem(m_log);
+    }
+
+    @Override
+    public void periodic() {
+        m_bottomPivot.periodic();
+        m_telescope.periodic();
+        m_topPivot.periodic();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        m_bottomPivot.simulate();
+        m_telescope.simulate();
+        m_topPivot.simulate();
+    }
+
+    public void setDesiredState(double desiredBottomAngle, double desiredBottomSpeed, double desiredExtension, 
+        double desiredExtensionSpeed, double desiredTopAngle, double desiredTopSpeed) {
+
+        m_bottomPivot.setAngle(desiredBottomAngle, desiredBottomSpeed);
+        m_telescope.setExtension(desiredExtension, desiredExtensionSpeed);
+        m_topPivot.setAngle(desiredTopAngle, desiredTopSpeed);
+    }
+}
