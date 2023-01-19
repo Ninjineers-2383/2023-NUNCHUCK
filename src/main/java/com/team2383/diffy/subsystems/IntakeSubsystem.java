9b06c3d1095c8ef66f626d4a8ffbdae32e7337a8
RisingNinjas16391 <ninjineers@ahschool.com>
2023-01-19 16:43:01 -0500

    Update IntakeSubsystem.java
    Co-Authored-By: ethandzt21 <102374149+ethandzt21@users.noreply.github.com>

--- src/main/java/com/team2383/diffy/subsystems/IntakeSubsystem.java
+++ src/main/java/com/team2383/diffy/subsystems/IntakeSubsystem.java
@@ -2,7 +2,8 @@ package com.team2383.diffy.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.team2383.diffy.Constants.FeederConstants;
+import com.revrobotics.REVPhysicsSim;
+import com.team2383.diffy.Constants.IntakeConstants;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.Nat;
@@ -14,11 +15,13 @@ import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.LinearSystemLoop;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class FeederSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
     //TODO: Comment
     private final CANSparkMax m_topMotor;
     private final CANSparkMax m_bottomMotor;
     private final CANSparkMax m_clawMotor;
+
+    private final 
 
     private final LinearSystem<N3, N3, N3> m_feederPlant;
     private final LinearQuadraticRegulator<N3, N3, N3> m_controller;
@@ -26,33 +29,35 @@ public class FeederSubsystem extends SubsystemBase {
     private final LinearSystemLoop<N3, N3, N3> m_systemLoop;
 
 
-    public FeederSubsystem() {
-        m_topMotor = new CANSparkMax(FeederConstants.kTopMotorID, MotorType.kBrushless);
-        m_bottomMotor = new CANSparkMax(FeederConstants.kBottomMotorID, MotorType.kBrushless);
-        m_clawMotor = new CANSparkMax(FeederConstants.kClawMotorID, MotorType.kBrushless);
+    public IntakeSubsystem() {
+        m_topMotor = new CANSparkMax(IntakeConstants.kTopMotorID, MotorType.kBrushless);
+        m_bottomMotor = new CANSparkMax(IntakeConstants.kBottomMotorID, MotorType.kBrushless);
+        m_clawMotor = new CANSparkMax(IntakeConstants.kClawMotorID, MotorType.kBrushless);
+
+        // RevPhysicsSim.getInstance().addSparkMax(m_topMotor, MotorType.kBrushless);
 
         m_feederPlant = new LinearSystem<>(
             // A Matrix
             Matrix.mat(Nat.N3(), Nat.N3()).fill(
-                -FeederConstants.kV / FeederConstants.kA, 0, 0,
-                0, -FeederConstants.kV / FeederConstants.kA, 0,
-                0, 0, -FeederConstants.kV / FeederConstants.kA),
+                -IntakeConstants.kV / IntakeConstants.kA, 0, 0,
+                0, -IntakeConstants.kV / IntakeConstants.kA, 0,
+                0, 0, -IntakeConstants.kV / IntakeConstants.kA),
 
             // B Matrix
             Matrix.mat(Nat.N3(), Nat.N3()).fill(
-                1 / FeederConstants.kA, 0, 0,
-                0, 1 / FeederConstants.kA, 0,
-                0, 0, 1 / FeederConstants.kA),
+                1 / IntakeConstants.kA, 0, 0,
+                0, 1 / IntakeConstants.kA, 0,
+                0, 0, 1 / IntakeConstants.kA),
 
             // C Matrix
             Matrix.mat(Nat.N3(), Nat.N3()).fill(
-                1, 0, 0,
+        1, 0, 0,
                 0, 1, 0,
                 0, 0, 1), 
 
             // D Matrix
             Matrix.mat(Nat.N3(), Nat.N3()).fill(
-                0, 0, 0,
+        0, 0, 0,
                 0, 0, 0,
                 0, 0, 0)
         );
@@ -66,8 +71,21 @@ public class FeederSubsystem extends SubsystemBase {
         m_systemLoop = new LinearSystemLoop<>(m_feederPlant, m_controller, 
             m_observer, 12.0, 0.02);
     }
+
     public void periodic() {
      
+    }
+
+    public void simulate() {
+        // m_leftMotorSim.setIntegratedSensorVelocity((int) ((m_systemLoop.getXHat(0) / (2 * Math.PI)) * 2048 / 10.0));
+        // m_rightMotorSim.setIntegratedSensorVelocity((int) ((m_systemLoop.getXHat(1) / (2 * Math.PI)) * 2048 / 10.0));
+
+        // SmartDashboard.putNumber("Simulated Left Motor Output Velocity",
+        //         m_leftMotor.getSelectedSensorVelocity());
+
+        // SmartDashboard.putNumber("Simulated Right Motor Output Velocity",
+        //         m_rightMotor.getSelectedSensorVelocity());
+
     }
 
     public void setPower(double top, double bottom) {
