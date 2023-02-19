0b591ee75397a92bad6ea114f3734307d8b939c1
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2023-02-19 01:08:49 -0500

    removed unused axis

--- src/main/java/com/team2383/diffy/subsystems/pinkArm/feeder/FeederSubsystem.java
+++ src/main/java/com/team2383/diffy/subsystems/pinkArm/feeder/FeederSubsystem.java
@@ -7,7 +7,6 @@ import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class FeederSubsystem extends SubsystemBase {
     private final Ninja_CANSparkMax m_topMotor;
