c68d736634e16598dbb02b46b5ec049bc41cbe68
RisingNinjas16391 <ninjineers@ahschool.com>
2023-02-23 15:09:04 -0500

    Forgot to save lol

---
+++ src/main/java/com/team2383/diffy/commands/pinkArm/position/PositionConstants.java
@@ -0,0 +1,31 @@
+package com.team2383.diffy.commands.pinkArm.position;
+
+public class PositionConstants {
+    
+    public class PivotPositionConstants {
+        // TODO: ADD PIVOT POSITIONS
+        public static final int kFeedGroundPos = 0;
+        public static final int kFeedPaddlePos = 0;
+        public static final int kTopScorePos = 0;
+        public static final int kMidScorePos = 0;
+        public static final int kLowScorePos = 0;
+
+    }
+
+    public class ExtensionPositionConstants {
+        // TODO: ADD EXTENSION POSITIONS
+        public static final int kRetracted = 0;
+        public static final int kExtended = 0;
+
+    }
+
+    public class WristPositionConstants {
+        // TODO: ADD WRIST POSITIONS
+        public static final int kFeedGroundPos = 0;
+        public static final int kFeedPaddlePos = 0;
+        public static final int kTopScorePos = 0;
+        public static final int kMidScorePos = 0;
+        public static final int kLowScorePos = 0;
+
+    }
+}
