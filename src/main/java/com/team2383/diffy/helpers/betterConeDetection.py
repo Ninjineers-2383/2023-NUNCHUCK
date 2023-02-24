9f56a1d55a2e11124365f77bbda4fecf3b58e378
Millenium-Falcon-NOT-Garbage <55559153+Millenium-Falcon-NOT-Garbage@users.noreply.github.com>
2023-02-23 22:02:15 -0500

    Camera Vision Tuning - in progress

--- src/main/java/com/team2383/diffy/helpers/betterConeDetection.py
+++ src/main/java/com/team2383/diffy/helpers/betterConeDetection.py
@@ -6,10 +6,10 @@ from networktables import NetworkTables
 logging.basicConfig(level=logging.DEBUG)
 
 NetworkTables.initialize()
-sd = NetworkTables.getTable("SmartDashboard")
+SmartDashboard = NetworkTables.getTable("SmartDashboard")
 
 # image = cv2.imread('/Users/john/FRC 2023/2023-Diffy-Swerve/src/main/java/com/team2383/diffy/helpers/SampleImages/img1.png')
-cap = cv2.VideoCapture(0) # TODO: RENAME TO CONE CAMERA NAME OR WHATEVER INDEX IT NEEDS TO BE
+cap = cv2.VideoCapture(0) # 0 is the default camera
 
 while cap.isOpened():
     
@@ -23,35 +23,65 @@ while cap.isOpened():
     
     original = frame.copy()
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    blur = cv2.GaussianBlur(image, (3, 3), sigmaX=1.5, borderType=cv2.BORDER_DEFAULT)
-    lower = np.array([24, 93, 20], dtype="uint8")
-    upper = np.array([29, 255, 255], dtype="uint8")
+    blur = cv2.GaussianBlur(image, (5, 5), sigmaX=1.5, borderType=cv2.BORDER_DEFAULT)
+    lower = np.array([24, 53, 30], dtype="uint8")
+    upper = np.array([32, 255, 255], dtype="uint8")
     mask = cv2.inRange(blur, lower, upper)
 
-    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
 
-    for c in cnts:
-        x,y,w,h = cv2.boundingRect(c)
-        # cv2.drawContours(original, [c], -1, (36,255,12), 5)
-        if(w > 60 and h > 60):
-            cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 5)
-            cv2.rectangle(original, (x + int(w/2) + 30, y + int(h/2) + 30), (x + int(w/2) - 30, y + int(h/2) - 30), (0,128,255), -1)
+    for contour in contours:
+        
+        approx = cv2.approxPolyDP(contour, 0.05 * cv2.arcLength(contour, True), True)
+        kx = approx.ravel()[0]
+        ky = approx.ravel()[1]
+        
+        if(len(approx) == 4):
+            # x,y,w,h = cv2.boundingRect(contour)
+            cv2.drawContours(original, [approx], 0, (36,255,12), 5)
             
-            print("Cone Center X: " + str(x + int(w/2) - width/2))
-            print("Cone Center Y: " + str(y + int(h/2) - height/2))
+            n = approx.ravel()
+            i = 0
             
-            sd.putNumber("Cone Center X", str(x + int(w/2) - width/2))
-            sd.putNumber("Cone Center Y", str(y + int(h/2) - height/2))
+            for j in n :
+                if(i % 2 == 0):
+                    x = n[i]
+                    y = n[i + 1]
+        
+                    # String containing the co-ordinates.
+                    string = str(x) + " " + str(y) 
+        
+                    if(i == 0):
+                        # text on topmost co-ordinate.
+                        cv2.putText(original, "Arrow tip", (x, y),
+                                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 0)) 
+                    else:
+                        # text on remaining co-ordinates.
+                        cv2.putText(original, string, (x, y), 
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0)) 
+                i = i + 1
+            
+            # if(w > 60 and h > 60):
+            #     # Rectangle around cone
+            #     cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 5)
+            #     # Rectangle that indicates the center of the cone
+            #     cv2.rectangle(original, (x + int(w/2) + 30, y + int(h/2) + 30), (x + int(w/2) - 30, y + int(h/2) - 30), (0,128,255), -1)
+                
+            #     # Distance indicator to determine how far the cone is from the camera
+            #     distanceIndicator = h
+            #     print("Distance: " + str(h))
+                
+            #     # SmartDashboard values
+            #     SmartDashboard.putNumber("Cone Center X", str(x + int(w/2) - width/2))
+            #     SmartDashboard.putNumber("Cone Center Y", str(y + int(h/2) - height/2))
+            #     SmartDashboard.putNumber("Cone Distance Indicator", distanceIndicator)
     
-    cv2.imshow("Stream", original)
+    cv2.imshow("Stream", mask)
+
+    if cv2.waitKey(0) and 0xFF == ord('w'):
+        break
+
             
 cap.release()
 cv2.destroyAllWindows()
-
-    # cv2.imshow('mask', mask)
-    # cv2.imshow('Blurred', blur)
-    # cv2.imshow('original', original)
-
-    # if cv2.waitKey(0) & 0xFF == ord('enter'):
-    # 	cv2.destroyAllWindows()
--- src/main/java/com/team2383/diffy/helpers/coneDetection.py
+++ src/main/java/com/team2383/diffy/helpers/coneDetection.py
@@ -2,68 +2,20 @@ import cv2
 import numpy as np
 import glob
 
-# cap = cv2.VideoCapture(0)
-images = [cv2.imread(file) for file in glob.glob(
-    "/Users/john/FRC 2023/2023-Diffy-Swerve/src/main/java/com/team2383/diffy/helpers/SampleImages/*.png")]
+cap = cv2.VideoCapture(0)
 
-for image in images:
-    # copy = image.copy()
-    # ret, thresh = cv2.adaptiveThreshold(image, )
-    # cv2.imshow('thresh', thresh)
-    # cv2.waitKey(0)
-    # if cv2.waitKey and 0xFF == ord('enter'):
-    #    break
-    img_copy = image.copy()
-    gaussianFilter = cv2.GaussianBlur(img_copy, (7, 7), 0)
-
-    # yellow filtering
-    hsv = cv2.cvtColor(gaussianFilter, cv2.COLOR_BGR2HSV)
-    lowerYellow = np.array([134, 170, 0])
-    upperYellow = np.array([240, 226, 182])
-
-    mask = cv2.inRange(hsv, lowerYellow, upperYellow)
-    rgbFilter = cv2.bitwise_and(image, image, mask=mask)
-    rgbFilter = cv2.normalize(src=rgbFilter, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    # detecting cone
-    grayscale = cv2.cvtColor(rgbFilter, cv2.COLOR_RGB2GRAY)
-    coneDetect = cv2.HoughCircles(grayscale, cv2.HOUGH_GRADIENT,1,100,minRadius=0,maxRadius=0)  # edge image, thresholding val, region size detecting peaks
-
-    circles = np.uint16(np.around(coneDetect))
-# Draw the circles
-    for i in circles[0,:]:
-    # draw the outer circle
-        cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-        cv2.circle(image,(i[0],i[1]),2,(0,0,255),3)
-    # print(grayscale.shape)
-    # showing image
-    cv2.imshow("edge rgb", image)
-
-    if cv2.waitKey(1) and 0xFF == ord('q'):
+while cap.isOpened():
+    
+    ret, frame = cap.read()
+    
+    if frame is None:
+        break
+    
+    cv2.imshow("Stream", frame)
+    
+    if cv2.waitKey(0) and 0xFF == ord('q'):
         break
 
-
-# while cap.isOpened():
-#     original = image.copy()
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-#     lower = np.array([22, 93, 0], dtype="uint8") # Lower HSV values
-#     upper = np.array([45, 255, 255], dtype="uint8") # Higher HSV values
-#     mask = cv2.inRange(image, lower, upper)
-
-#     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Contours the shape
-#     contours = contours[0] if len(contours) == 2 else contours[1]
-
-#     for contour in contours:
-#         # x,y,w,h = cv2.boundingRect(contour)
-#         cv2.drawContours(original, [contour], -1, (36,255,12), 2) # Draws a contour around the shape
-#         # cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 2) # Draws a rectangle around the shape
-
-#     cv2.imshow('mask', mask)
-#     cv2.imshow('original', original)
-#     cv2.waitKey()
-
-#     if cv2.waitKey and 0xFF ==ord('enter'):
-#         break
-
-# cap.release()
+            
+cap.release()
 cv2.destroyAllWindows()
