a81e77afb681468db40a802f47d784a6ec7adffc
Millenium-Falcon-NOT-Garbage <55559153+Millenium-Falcon-NOT-Garbage@users.noreply.github.com>
2023-02-23 20:39:01 -0500

    Finalized Camera Vision

--- src/main/java/com/team2383/diffy/helpers/betterConeDetection.py
+++ src/main/java/com/team2383/diffy/helpers/betterConeDetection.py
@@ -9,16 +9,23 @@ NetworkTables.initialize()
 sd = NetworkTables.getTable("SmartDashboard")
 
 # image = cv2.imread('/Users/john/FRC 2023/2023-Diffy-Swerve/src/main/java/com/team2383/diffy/helpers/SampleImages/img1.png')
-cap = cv2.VideoCapture('ConeCamera') # TODO: RENAME TO CONE CAMERA NAME OR WHATEVER INDEX IT NEEDS TO BE
+cap = cv2.VideoCapture(0) # TODO: RENAME TO CONE CAMERA NAME OR WHATEVER INDEX IT NEEDS TO BE
 
 while cap.isOpened():
     
-    image = cap.read()
-    original = image.copy()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    ret, frame = cap.read()
+    
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    
+    if frame is None:
+        break
+    
+    original = frame.copy()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     blur = cv2.GaussianBlur(image, (3, 3), sigmaX=1.5, borderType=cv2.BORDER_DEFAULT)
     lower = np.array([24, 93, 20], dtype="uint8")
-    upper = np.array([43, 255, 255], dtype="uint8")
+    upper = np.array([29, 255, 255], dtype="uint8")
     mask = cv2.inRange(blur, lower, upper)
 
     cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -29,12 +36,15 @@ while cap.isOpened():
         # cv2.drawContours(original, [c], -1, (36,255,12), 5)
         if(w > 60 and h > 60):
             cv2.rectangle(original, (x, y), (x + w, y + h), (36,255,12), 5)
-            print("Width: " + str(w))
-            print("Height: " + str(h))
             cv2.rectangle(original, (x + int(w/2) + 30, y + int(h/2) + 30), (x + int(w/2) - 30, y + int(h/2) - 30), (0,128,255), -1)
             
-            sd.putNumber("Cone Center X: " + str(x + int(w/2)))
-            sd.putNumber("Cone Center Y: " + str(y + int(h/2)))
+            print("Cone Center X: " + str(x + int(w/2) - width/2))
+            print("Cone Center Y: " + str(y + int(h/2) - height/2))
+            
+            sd.putNumber("Cone Center X", str(x + int(w/2) - width/2))
+            sd.putNumber("Cone Center Y", str(y + int(h/2) - height/2))
+    
+    cv2.imshow("Stream", original)
             
 cap.release()
 cv2.destroyAllWindows()
