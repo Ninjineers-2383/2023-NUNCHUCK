58c839d83a25d693aedab169840dee92371eb6ee
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2023-02-28 14:15:59 -0500

    resolved issues due to pullling bad code

--- src/main/java/com/team2383/diffy/autos/AutoSettings.java
+++ src/main/java/com/team2383/diffy/autos/AutoSettings.java
@@ -26,64 +26,64 @@ public class AutoSettings {
 
     private static DrivetrainSubsystem m_drivetrainSubsystem;
 
-    public static final HashMap<String, Command> autoHashMap = new HashMap<>() {
-        {
-            put("Auto Log", new PrintCommand("Auto Event: log"));
+    // public static final HashMap<String, Command> autoHashMap = new HashMap<>() {
+    //     {
+    //         put("Auto Log", new PrintCommand("Auto Event: log"));
 
-            put("Feed Cone",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.FEED_UPRIGHT_CONE),
-                            new FeederCommand(m_feederSubsystem, () -> 1).withTimeout(0.7)));
-            put("Feed Cube",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.FEED_CONE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> 1).withTimeout(0.7)));
+    //         put("Feed Cone",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.FEED_UPRIGHT_CONE),
+    //                         new FeederCommand(m_feederSubsystem, () -> 1).withTimeout(0.7)));
+    //         put("Feed Cube",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.FEED_CONE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> 1).withTimeout(0.7)));
 
-            put("Score Cone Low",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.LOW_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
-            put("Score Cone Mid",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.MID_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
-            put("Score Cone High",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.HIGH_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
+    //         put("Score Cone Low",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.LOW_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
+    //         put("Score Cone Mid",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.MID_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
+    //         put("Score Cone High",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.HIGH_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -1).withTimeout(0.7)));
 
-            put("Score Cube Low",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.LOW_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
-            put("Score Cube Mid",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.MID_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
-            put("Score Cube High",
-                    new SequentialCommandGroup(
-                            new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
-                                    PositionConstants.HIGH_SCORE_POS),
-                            new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
-        }
-    };
+    //         put("Score Cube Low",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.LOW_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
+    //         put("Score Cube Mid",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.MID_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
+    //         put("Score Cube High",
+    //                 new SequentialCommandGroup(
+    //                         new PinkArmPresetCommand(m_pivotSubsystem, m_telescopeSubsystem, m_wristSubsystem,
+    //                                 PositionConstants.HIGH_SCORE_POS),
+    //                         new FeederCommand(m_feederSubsystem, () -> -0.6).withTimeout(0.7)));
+    //     }
+    // };
 
-    public static SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
-            m_drivetrainSubsystem::getPose,
-            m_drivetrainSubsystem::forceOdometry,
-            m_drivetrainSubsystem.m_kinematics,
-            new PIDConstants(5, 0, 0),
-            new PIDConstants(0.5, 0, 0),
-            m_drivetrainSubsystem::setModuleStates,
-            AutoSettings.autoHashMap,
-            true,
-            m_drivetrainSubsystem);
+    // public static SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+    //         m_drivetrainSubsystem::getPose,
+    //         m_drivetrainSubsystem::forceOdometry,
+    //         m_drivetrainSubsystem.m_kinematics,
+    //         new PIDConstants(5, 0, 0),
+    //         new PIDConstants(0.5, 0, 0),
+    //         m_drivetrainSubsystem::setModuleStates,
+    //         AutoSettings.autoHashMap,
+    //         true,
+    //         m_drivetrainSubsystem);
 
 }
